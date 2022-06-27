46291050057361ea27d7a080acbf5705c4146097
Никита Ульянов <nik.ule@yandex.ru>
2022-06-27 15:22:47 +0300

    LinkedList

---
+++ src/List/LinkedList.py
@@ -0,0 +1,127 @@
+class Node:
+
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+
+class LinkedList:
+    size = 0
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        self.size += 1
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        result = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                result.append(node)
+            node = node.next
+        return result
+
+    def delete(self, val, all=False):
+        if self.size != 0:
+            last_node = self.head
+            if last_node.value == val:
+                if self.size == 1:
+                    self.clean()
+                    return
+                else:
+                    self.head = last_node.next
+                    self.size -= 1
+                    if not all:
+                        return
+                    else:
+                        return self.delete(val, all)
+            node = last_node.next
+            while node is not None:
+                if node.value == val:
+                    if node == self.tail:
+                        self.tail = last_node
+                        last_node.next = None
+                    else:
+                        last_node.next = node.next
+                    self.size -= 1
+                    if not all:
+                        return
+                    else:
+                        return self.delete(val, all)
+                last_node = node
+                node = node.next
+
+    def clean(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def len(self):
+        return self.size
+
+    def insert(self, afterNode, newNode):
+        if afterNode is not None:
+            newNode.next = afterNode.next
+            afterNode.next = newNode
+            if afterNode == self.tail:
+                self.tail = afterNode
+        else:
+            newNode.next = self.head
+            self.head = newNode
+            if self.size == 0:
+                self.tail = newNode
+        self.size += 1
+
+    def equals(self, another):
+        if self.size != another.size:
+            return False
+        if self.size == 0 and another.size == 0:
+            return True
+        if self.head.value != another.head.value:
+            return False
+        if self.tail.value != another.tail.value:
+            return False
+        a_node = self.head
+        b_node = another.head
+        while a_node is not None and b_node is not None:
+            if a_node.value != b_node.value:
+                return False
+            a_node = a_node.next
+            b_node = b_node.next
+        return True
+
+    @staticmethod
+    def sum_list(a_list, b_list):
+        if a_list.len() == b_list.len() and a_list.len != 0:
+            result = LinkedList()
+            a_node = a_list.head
+            b_node = b_list.head
+            while a_node is not None:
+                result.add_in_tail(Node(a_node.value + b_node.value))
+                a_node = a_node.next
+                b_node = b_node.next
+            return result
+        elif a_list.len == 0 and b_list.len == 0:
+            return LinkedList()
---
+++ src/List/Test_LinkedList.py
@@ -0,0 +1,197 @@
+import unittest
+
+from src.List.LinkedList import Node, LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    def test_delete(self):
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(-5))
+        correct_list.add_in_tail(Node(0))
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(-5))
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(128))
+        test_list.delete(128)
+
+        self.assertTrue(correct_list.equals(test_list))
+        self.assertEqual(test_list.len(), 2)
+
+        correct_list = LinkedList()
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(0))
+        test_list.delete(0)
+
+        self.assertTrue(correct_list.equals(test_list))
+        self.assertEqual(test_list.len(), 0)
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(0))
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(1))
+        test_list.delete(1)
+
+        self.assertIsNotNone(test_list.head)
+        self.assertIsNotNone(test_list.tail)
+        self.assertTrue(correct_list.equals(test_list))
+        self.assertEqual(test_list.len(), 1)
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(0))
+        correct_list.add_in_tail(Node(10))
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(5))
+        test_list.add_in_tail(Node(10))
+        test_list.delete(5)
+
+        self.assertTrue(correct_list.equals(test_list))
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(0))
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(0))
+        test_list.delete(0)
+
+        self.assertTrue(correct_list.equals(test_list))
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(1))
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(1))
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(0))
+        test_list.delete(0, True)
+
+        self.assertTrue(correct_list.equals(test_list))
+
+    def test_clean(self):
+        correct_list = LinkedList()
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(0))
+        test_list.add_in_tail(Node(5))
+        test_list.add_in_tail(Node(0))
+        test_list.clean()
+
+        self.assertTrue(correct_list.equals(test_list))
+
+    def test_find_all(self):
+        a = Node(0)
+        b = Node(5)
+        c = Node(0)
+
+        test_list = LinkedList()
+        test_list.add_in_tail(a)
+        test_list.add_in_tail(b)
+        test_list.add_in_tail(c)
+
+        self.assertEqual(test_list.find_all(0), [a, c])
+        self.assertEqual(test_list.find_all(0)[0], a)
+        self.assertEqual(test_list.find_all(0)[1], c)
+
+    def test_len(self):
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(1))
+        test_list.add_in_tail(Node(5))
+        test_list.delete(1)
+        test_list.insert(test_list.find(5), Node(8))
+        self.assertEqual(test_list.len(), 2)
+
+    def test_insert(self):
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(1))
+        test_list.add_in_tail(Node(5))
+        test_list.add_in_tail(Node(10))
+        test_list.insert(test_list.find(5), Node(8))
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(1))
+        correct_list.add_in_tail(Node(5))
+        correct_list.add_in_tail(Node(8))
+        correct_list.add_in_tail(Node(10))
+        self.assertTrue(correct_list.equals(test_list))
+
+        node = Node(5)
+        test_list = LinkedList()
+        test_list.insert(None, node)
+        self.assertEqual(test_list.head, node)
+        self.assertEqual(test_list.tail, node)
+        self.assertEqual(test_list.len(), 1)
+
+        node1 = Node(5)
+        node2 = Node(7)
+        test_list = LinkedList()
+        test_list.insert(None, node2)
+        test_list.insert(None, node1)
+        self.assertEqual(test_list.head, node1)
+        self.assertEqual(test_list.tail, node2)
+        self.assertEqual(test_list.len(), 2)
+
+    def test_equals(self):
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(1))
+        test_list.add_in_tail(Node(5))
+        test_list.add_in_tail(Node(10))
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(1))
+        correct_list.add_in_tail(Node(5))
+        correct_list.add_in_tail(Node(10))
+        self.assertTrue(correct_list.equals(test_list))
+
+        test_list = LinkedList()
+
+        correct_list = LinkedList()
+        self.assertTrue(correct_list.equals(test_list))
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(5))
+
+        correct_list = LinkedList()
+        self.assertFalse(correct_list.equals(test_list))
+
+    def test_sum_list(self):
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(1))
+
+        correct_list = LinkedList()
+        self.assertIsNone(LinkedList.sum_list(test_list, correct_list))
+
+        test_list = LinkedList()
+
+        correct_list = LinkedList()
+        self.assertIsNotNone(LinkedList.sum_list(test_list, correct_list))
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(15))
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(30))
+        self.assertEqual(LinkedList.sum_list(test_list, correct_list).head.value, 45)
+        self.assertEqual(LinkedList.sum_list(test_list, correct_list).tail.value, 45)
+        self.assertEqual(LinkedList.sum_list(test_list, correct_list).len(), 1)
+
+        test_list = LinkedList()
+        test_list.add_in_tail(Node(15))
+        test_list.add_in_tail(Node(20))
+        test_list.add_in_tail(Node(-100))
+
+        correct_list = LinkedList()
+        correct_list.add_in_tail(Node(30))
+        correct_list.add_in_tail(Node(60))
+        correct_list.add_in_tail(Node(80))
+        self.assertEqual(LinkedList.sum_list(test_list, correct_list).head.value, 45)
+        self.assertEqual(LinkedList.sum_list(test_list, correct_list).head.next.value, 80)
+        self.assertEqual(LinkedList.sum_list(test_list, correct_list).tail.value, -20)
+        self.assertEqual(LinkedList.sum_list(test_list, correct_list).len(), 3)
