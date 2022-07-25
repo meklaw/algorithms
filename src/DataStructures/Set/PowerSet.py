88f6bc3cd17ca69b12ea3e486f87ce7c6db07b7f
Никита Ульянов <nik.ule@yandex.ru>
2022-07-25 17:39:54 +0300

    add PowerSet.py

--- src/DataStructures/Set/PowerSet.py
+++ src/DataStructures/Set/PowerSet.py
@@ -1,124 +1,131 @@
 class PowerSet:
 
     def __init__(self):
-        self.__len = 0
-        self.__capacity = 20_000
-        self.slots = [None] * self.__capacity
-
-    def hash_fun(self, value: str):
-        h = 0
-        m = 1
-        for c in value:
-            x = ord(c)
-            h = (h + m * x) % self.__capacity
-            m = (m * 91) % self.__capacity
-        return h
-
-    def seek_slot(self, value):
-        h = self.hash_fun(value)
-        if self.slots[h] == value:
-            return None
-        step = 3
-        if self.__capacity % step == 0:
-            step += 1
-        c = 0
-        while self.slots[h] is not None:
-            h = (h + step) % self.__capacity
-            c += 1
-            if c > self.__capacity:
-                return None
-        return h
+        self.slots = []
 
     def size(self):
-        return self.__len
+        return len(self.slots)
 
-    def put(self, key):
-        h = self.seek_slot(key)
-        if h is None:
-            return None
-        self.slots[h] = key
-        self.__len += 1
+    def put(self, value):
+        if self.size() == 0:
+            self.slots.append(value)
+            return
+        if self.get(value):
+            return
+        if value < self.slots[0]:
+            self.slots.insert(0, value)
+            return
+        if self.slots[self.size() - 1] < value:
+            self.slots.append(value)
+            return
+        a = 0
+        b = self.size() - 1
+        medium = self.size() // 2
+        while True:
+            if value < self.slots[medium]:
+                b = medium
+            if self.slots[medium] < value:
+                a = medium
+            if (b - a) == 1:
+                self.slots.insert(b, value)
+                return
+            medium = (a + b) // 2
 
     def get(self, value):
-        if self.__get_id(value) >= 0:
+        if self.size() == 0:
+            return False
+        if value == self.slots[0]:
             return True
-        return False
-
-    def __get_id(self, value):
-        h = self.hash_fun(value)
-        if self.slots[h] == value:
-            return h
-
-        step = 3
-        if self.__capacity % step == 0:
-            step += 1
-        c = 0
-        while self.slots[h] != value:
-            h = (h + step) % self.__capacity
-            c += 1
-            if c > self.__capacity:
-                return -1
-        return h
+        if self.slots[self.size() - 1] == value:
+            return True
+        if value < self.slots[0]:
+            return False
+        if self.slots[self.size() - 1] < value:
+            return False
+        a = 0
+        b = self.size() - 1
+        medium = self.size() // 2
+        while True:
+            if value < self.slots[medium]:
+                b = medium
+            if self.slots[medium] < value:
+                a = medium
+            if self.slots[medium] == value:
+                return True
+            if (b - a) == 1:
+                if self.slots[a] == value:
+                    return True
+                if self.slots[b] == value:
+                    return True
+                if self.slots[medium] == value:
+                    return True
+                return False
+            medium = (a + b) // 2
 
     def remove(self, value):
-        h = self.__get_id(value)
-        if h >= 0:
-            self.slots[h] = None
-            self.__len -= 1
-            return True
-        return False
-
-    def get_values(self):
-        res = []
         if self.size() == 0:
-            return res
-        for i in self.slots:
-            if i is not None:
-                res.append(i)
-        return res
+            return False
+        if value == self.slots[0]:
+            self.slots.pop(0)
+            return True
+        if self.slots[self.size() - 1] == value:
+            self.slots.pop(self.size() - 1)
+            return True
+        if value < self.slots[0]:
+            return False
+        if self.slots[self.size() - 1] < value:
+            return False
+        a = 0
+        b = self.size() - 1
+        medium = self.size() // 2
+        while True:
+            if value < self.slots[medium]:
+                b = medium
+            if self.slots[medium] < value:
+                a = medium
+            if self.slots[medium] == value:
+                self.slots.pop(medium)
+                return True
+            if (b - a) == 1:
+                if self.slots[a] == value:
+                    self.slots.pop(a)
+                    return True
+                if self.slots[b] == value:
+                    self.slots.pop(b)
+                    return True
+                if self.slots[medium] == value:
+                    self.slots.pop(medium)
+                    return True
+                return False
+            medium = (a + b) // 2
 
     def intersection(self, set2):
         result = PowerSet()
-        if self.size() == 0 and set2.size() == 0:
-            return result
-        a = self.get_values()
-        b = set2.get_values()
-        for i in a:
-            for k in b:
-                if i == k:
-                    result.put(i)
-                    break
+        for i in self.slots:
+            if set2.get(i):
+                result.put(i)
         return result
 
     def union(self, set2):
         result = PowerSet()
-        if self.size() == 0 and set2.size() == 0:
-            return result
-        a = self.get_values()
-        b = set2.get_values()
-        for i in a:
+        for i in self.slots:
             result.put(i)
-        for i in b:
+        for i in set2.slots:
             result.put(i)
         return result
 
     def difference(self, set2):
         result = PowerSet()
-        if self.size() == 0 and set2.size() == 0:
-            return result
-        a = self.get_values()
-        b = set2.get_values()
-        for i in a:
-            if set2.__get_id(i) == -1:
+        for i in self.slots:
+            if not set2.get(i):
                 result.put(i)
-        for i in b:
-            if self.__get_id(i) == -1:
+        for i in set2.slots:
+            if not self.get(i):
                 result.put(i)
         return result
 
     def issubset(self, set2):
-        b = set2.get_values()
-        for i in b:
-            if self.__get_id(i) == -1:
+        for i in set2.slots:
+            if not self.get(i):
                 return False
         return True
