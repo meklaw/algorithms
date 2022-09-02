06c46d68d1396c9a90893dbc479df0ddd8e26d05
Никита Ульянов <nik.ule@yandex.ru>
2022-09-02 21:05:20 +0300

    fix: try replaced on if

--- src/Algorithms/recursion/recursion_5.py
+++ src/Algorithms/recursion/recursion_5.py
@@ -1,10 +1,10 @@
 def print_even_elements(array: list):
+    if len(array) == 0:
+        return
     try:
         element = array.pop()
         if element % 2 == 0:
             print(element)
-    except IndexError:
-        return
     except TypeError:
         pass
     print_even_elements(array)
--- src/Algorithms/recursion/recursion_6.py
+++ src/Algorithms/recursion/recursion_6.py
@@ -1,7 +1,6 @@
 def print_every_second(array: list):
-    try:
-        array.pop(0)
-        print(array.pop(0))
-    except IndexError:
+    if len(array) < 2:
         return
+    array.pop(0)
+    print(array.pop(0))
     print_every_second(array)
